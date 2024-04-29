6f9a32ad224c6923d43d5e1ce0bc291cab0641ad
HyunwooJu <soc05104@gmail.com>
2024-04-29 19:12:04 +0900

    Study update

--- 04.js
+++ 04.js
@@ -3,3 +3,15 @@
 // 클래스 --> 객체(인스턴스)
 // 프로퍼티(Constructor)
 // new Class(a, b, c)
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+// instance 생성
+const rect1 = new Rectangle(10, 20);
+const rect2 = new Rectangle(10, 30);
+const rect3 = new Rectangle(15, 20);
